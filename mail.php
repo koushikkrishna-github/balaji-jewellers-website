ce3c84339808016d425fd4ba90382482ca05303f
koushikkrishna-github <koushikkrishna.boddu@gmail.com>
2022-08-26 14:52:37 +0530

    mailer added

---
+++ mail.php
@@ -0,0 +1,37 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once "vendor/autoload.php";
+
+//PHPMailer Object
+$mail = new PHPMailer(true); //Argument true in constructor enables exceptions
+
+//From email address and name
+$mail->From = "from@gmail.com";
+$mail->FromName = "From Name";
+
+//To address and name
+$mail->addAddress("recepient1@example.com", "Recepient Name");
+// $mail->addAddress("recepient1@example.com"); //Recipient name is optional
+
+//Address to which recipient will reply
+// $mail->addReplyTo("reply@yourdomain.com", "Reply");
+
+//CC and BCC
+// $mail->addCC("cc@example.com");
+// $mail->addBCC("bcc@example.com");
+
+//Send HTML or Plain Text email
+$mail->isHTML(true);
+
+$mail->Subject = "Subject Text";
+$mail->Body = "Name: ".$_POST['name']."<br> Email: ".$_POST['email']." <br> Phone: ".$_POST['phone']." <br> Message: ".$_POST['message'];
+$mail->AltBody = "This is the plain text version of the email content";
+
+try {
+    $mail->send();
+    echo "Message has been sent successfully";
+} catch (Exception $e) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}
